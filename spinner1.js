a16d84b91edbb3d5196e2c99f57c3d8147fbb4b9
Yasin Hussein <bulaale116@gmail.com>
2023-11-23 08:29:32 -0700

    spinner1

---
+++ spinner1.js
@@ -0,0 +1,14 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+/* eslint-disable space-before-blocks */
+/* eslint-disable linebreak-style */
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+let word = 'CODING';
+ let backSlashes = ['|','/','-','\\','/','\\'];
+for (let i = 0; i < word.length; i++){
+  const timeDelay = 100 * (i + 1);
+setTimeout(() => {
+  process.stdout.write(`\r${backSlashes[i]}  ${word[i]} `);
+}, timeDelay);
+}
+
